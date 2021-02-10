f6f69e74eaf056f7ec7fa970a91d4a8c8353f6dd
Kasey Arnold <kaseyarnold15@gmail.com>
2021-02-10 20:22:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 /* Given Code, don't edit */
 
-document.querySelector("button").addEventListener("click", handleClick);
-
 function handleClick(e) {
   const timeString = document.getElementById('time').value
   displayMessage(greet(timeString))
@@ -11,3 +9,24 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(str){
+
+  if (str.length === 4){
+    str = '0'+ str;
+  }
+  if (str < "12:00"){
+    return 'Good Morning'
+  } else if (str > '17:00'){
+    return "Good Evening"
+  } else {
+    return "Good Afternoon"
+  }
+}
+
+function displayMessage(str){
+  // console.log(str)
+  let greeter = document.querySelector("#greeting").textContent = str
+  console.log(greeter)
+}
+ 1  node_modules/.bin/_mocha 
